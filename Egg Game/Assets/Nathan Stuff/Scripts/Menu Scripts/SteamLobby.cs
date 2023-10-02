fc2a4cbb820b2a0053e29a98ed3ec7e8627ab1ab
Nathan Mcfadyen <94457526+GamingGuy4321@users.noreply.github.com>
2023-10-02 13:50:15 -0300

    Lobby System is Set Up Mechanically

--- Egg Game/Assets/Nathan Stuff/Scripts/Menu Scripts/SteamLobby.cs
+++ Egg Game/Assets/Nathan Stuff/Scripts/Menu Scripts/SteamLobby.cs
@@ -10,12 +10,12 @@ public class SteamLobby : MonoBehaviour
 {
     public GameObject hostButton = null;
     public GameObject refreshButton;
-    public TMP_Dropdown lobbyDropdown;
+    public GameObject lobbyDropdown;
     public GameObject serverNameInputField;
     public GameObject serverPasswordInputField;
     public GameObject background;
     public Toggle passwordProtectedToggle;
-    public TMP_InputField serverPasswordJoinInputField;
+    public GameObject serverPasswordJoinInputField;
     public Toggle friendsOnlyToggle;
     public Toggle privateOnlyToggle;
 
@@ -49,12 +49,20 @@ public class SteamLobby : MonoBehaviour
 
     public void HostLobby()
     {
-        //background.SetActive(false);
+
         ELobbyType lobbyType;
 
         if (passwordProtectedToggle.isOn)
         {
-            Debug.Log("Password Public Lobby");
+            // Check if the password is empty or contains only spaces
+            string password = serverPasswordInputField.GetComponent<TMP_InputField>().text;
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                // Display an error message or handle it in your game's UI
+                Debug.LogError("Password cannot be empty or contain only spaces.");
+                return; // Exit the method without creating the lobby
+            }
+
             // Set it to the appropriate lobby type for password protection.
             lobbyType = ELobbyType.k_ELobbyTypePublic; // Or another appropriate type.
         }
@@ -62,7 +70,9 @@ public class SteamLobby : MonoBehaviour
         {
             Debug.Log("Friend Lobby");
             lobbyType = ELobbyType.k_ELobbyTypeFriendsOnly;
-        }else if (privateOnlyToggle.isOn){
+        }
+        else if (privateOnlyToggle.isOn)
+        {
             Debug.Log("Private Lobby");
             lobbyType = ELobbyType.k_ELobbyTypePrivate;
         }
@@ -72,8 +82,12 @@ public class SteamLobby : MonoBehaviour
             lobbyType = ELobbyType.k_ELobbyTypePublic;
         }
 
+        background.SetActive(false);
+        lobbyDropdown.SetActive(false);
+
         SteamMatchmaking.CreateLobby(lobbyType, customNetworkManager.maxConnections);
     }
+
 
 
     private void OnLobbyCreated(LobbyCreated_t callback)
@@ -136,7 +150,7 @@ public void RefreshLobbies()
 {
     Debug.Log("Looking for Lobbies");
     // Clear the existing dropdown options
-    lobbyDropdown.ClearOptions();
+    lobbyDropdown.GetComponent<TMP_Dropdown>().ClearOptions();
 
     // Request the lobby list from Steam
     SteamMatchmaking.RequestLobbyList();
@@ -145,23 +159,47 @@ public void RefreshLobbies()
     List<string> lobbyNames = new List<string>();
 
     // Add the lobby names to the dropdown
-    lobbyDropdown.AddOptions(lobbyNames);
+    lobbyDropdown.GetComponent<TMP_Dropdown>().AddOptions(lobbyNames);
 
     Debug.Log("Added lobby names to the dropdown.");
 }
 
+public void TogglePasswordInputField()
+{
+    // Get the currently selected lobby from the dropdown
+    int selectedIndex = lobbyDropdown.GetComponent<TMP_Dropdown>().value;
+
+    // Check if a valid lobby index is selected
+    if (selectedIndex >= 0 && selectedIndex < lobbyDropdown.GetComponent<TMP_Dropdown>().options.Count)
+    {
+        // Get the lobby name from the selected option
+        string selectedLobbyName = lobbyDropdown.GetComponent<TMP_Dropdown>().options[selectedIndex].text;
+
+        // Get the CSteamID for the selected lobby
+        CSteamID lobbyID = SteamMatchmaking.GetLobbyByIndex(selectedIndex);
+
+        // Check if this lobby is password-protected
+        bool isPasswordProtected = !string.IsNullOrEmpty(SteamMatchmaking.GetLobbyData(lobbyID, "Password"));
+
+        // Disable the password input field if the lobby is not password-protected
+        if (serverPasswordJoinInputField != null)
+        {
+            serverPasswordJoinInputField.SetActive(isPasswordProtected);
+        }
+    }
+}
 
 
 public void JoinSelectedLobby()
 {
     // Get the currently selected lobby from the dropdown
-    int selectedIndex = lobbyDropdown.value;
+    int selectedIndex = lobbyDropdown.GetComponent<TMP_Dropdown>().value;
 
     // Check if a valid lobby index is selected
-    if (selectedIndex >= 0 && selectedIndex < lobbyDropdown.options.Count)
+    if (selectedIndex >= 0 && selectedIndex < lobbyDropdown.GetComponent<TMP_Dropdown>().options.Count)
     {
         // Get the lobby name from the selected option
-        string selectedLobbyName = lobbyDropdown.options[selectedIndex].text;
+        string selectedLobbyName = lobbyDropdown.GetComponent<TMP_Dropdown>().options[selectedIndex].text;
         Debug.Log("Selected lobby name: " + selectedLobbyName);
 
         // Get the CSteamID for the selected lobby
@@ -186,13 +224,12 @@ public void JoinSelectedLobby()
                 Debug.Log("Selected lobby item name: " + selectedLobbyName);
 
                 // Prompt the player for the password
-                string enteredPassword = serverPasswordJoinInputField.text;
+                string enteredPassword = serverPasswordJoinInputField.GetComponent<TMP_InputField>().text;
                 Debug.Log("Entered password: " + enteredPassword);
 
                 if (enteredPassword == SteamMatchmaking.GetLobbyData(lobbyID, "Password"))
                 {
                     //background.SetActive(false);
-                    serverNameInputField.SetActive(false);
                     Debug.Log("Joining lobby: " + selectedLobbyName);
                     // Correct password entered, join the lobby
                     SteamMatchmaking.JoinLobby(lobbyID);
@@ -218,6 +255,9 @@ public void JoinSelectedLobby()
             SteamMatchmaking.JoinLobby(lobbyID);
         }
     }
+
+    // Call the function to toggle the password input field
+    TogglePasswordInputField();
 }
 
 
@@ -226,7 +266,7 @@ public void JoinSelectedLobby()
         int numLobbies = (int)callback.m_nLobbiesMatching;
 
         // Clear the existing dropdown options
-        lobbyDropdown.ClearOptions();
+        lobbyDropdown.GetComponent<TMP_Dropdown>().ClearOptions();
 
         for (int i = 0; i < numLobbies; i++)
         {
@@ -237,9 +277,12 @@ public void JoinSelectedLobby()
             if (!string.IsNullOrEmpty(lobbyName))
             {
                 // Add the lobby name to the dropdown
-                lobbyDropdown.AddOptions(new List<string> { lobbyName });
+                lobbyDropdown.GetComponent<TMP_Dropdown>().AddOptions(new List<string> { lobbyName });
 
             }
         }
+
+        // Call the function to toggle the password input field
+        TogglePasswordInputField();
     }
 }
